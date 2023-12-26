26287855ae6751994d506656311ae07372c742d5
Emma Reuter <emma@emmareuter.com>
2023-12-26 14:32:06 -0600

    rewrote tjnull to read from/write to database

--- tjnull.py
+++ tjnull.py
@@ -2,24 +2,31 @@ import tqdm
 from utils.data import *
 import random
 
-_, not_done = load_boxes()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.mysite.settings')
+import django
+django.setup()
+
+from mysite.htb.models import  *
+
 
 
 lines = open("data/tjnullstarter","r").read().split()
 
+boxes = Box.objects.filter(authUserInRootOwns=False)
+
 
 to_do = []
-for i in lines:
-    for j in not_done:
-        if i == j["name"]:
-            to_do.append(j)
+
+for i in boxes:
+    if i.name in lines:
+        to_do.append(i)
 
 tqdm.tqdm(initial=len(lines)-len(to_do),total=len(lines),desc="Percentage TJnull starter Done",unit="boxes",unit_scale=False)
 
 picked = random.choice(to_do)
 picked,now = load_start(picked)
-print("Do: ",picked['name'])
+print("Do: ",picked.name)
 guide = input("Did you use  a guide [1/0]?")
 
-write_done([picked['name']])
-write_times(now,guide,picked)
+picked.authUserInRootOwns = True
+picked.save()
